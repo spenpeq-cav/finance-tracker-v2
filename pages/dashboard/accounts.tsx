b07734fb4cd32935ba9b19bbcb604ba90ed3b1b3
Spencer Pequegnat <spencer.peq@gmail.com>
2022-04-01 16:08:54 -0700

    react-spinners. Added spinner to accounts page

--- pages/dashboard/accounts.tsx
+++ pages/dashboard/accounts.tsx
@@ -3,10 +3,12 @@ import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
 import AccessDenied from "../../components/AccessDenied";
 import Link from "next/link";
+import CircleLoader from "react-spinners/CircleLoader";
 
 const Accounts: NextPage = () => {
   const { data: session, status } = useSession();
   const loading = status === "loading";
+  const [dataLoading, setDataLoading] = useState<boolean>(true);
   const [accountsData, setAccountsData] = useState<any[]>([]);
   const [totalInst, setTotalInst] = useState<any>();
 
@@ -18,10 +20,10 @@ const Accounts: NextPage = () => {
       const data = await response.json();
       console.log(data);
       setAccountsData(data);
+      setDataLoading(false);
     };
 
     fetchData();
-
     setTotalInst(accountsData.length);
   }, [session, accountsData.length]);
 
@@ -78,6 +80,17 @@ const Accounts: NextPage = () => {
             <h1 className="text-slate-200 font-bold text-4xl text-left">
               {"Total Linked Institutions: " + totalInst}
             </h1>
+            {/* <BounceLoader color={"#a3e635"} loading={dataLoading} css={""} size={60} speedMultiplier={2} /> */}
+          </div>
+
+          <div className="col-span-4 text-center">
+            <CircleLoader
+              color={"#a3e635"}
+              loading={dataLoading}
+              css={""}
+              size={250}
+              speedMultiplier={1}
+            />
           </div>
 
           {accountsData &&
@@ -87,9 +100,10 @@ const Accounts: NextPage = () => {
                 className="border-2 border-lime-600 rounded-lg bg-slate-400 p-4 w-full col-span-4 text-left px-8"
               >
                 <h1 className="text-4xl py-4 font-bold text-slate-900 tracking-wider">
-                  {index + 1} | {acc.request_id} | {acc.accounts.length} | {acc.item.institution_id} 
+                  {index + 1} | {acc.request_id} | {acc.accounts.length} |{" "}
+                  {acc.item.institution_id}
                 </h1>
-                { acc.accounts.map((item: any, index: any) => (
+                {acc.accounts.map((item: any, index: any) => (
                   <p key={index}>
                     {item.name} | {item.account_id} | {item.balances.current}
                   </p>
